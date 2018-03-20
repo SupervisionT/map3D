03d6d68139fbda92472209e70be80c76ea54a7b6
root <root@LAPTOP-I5O7BKN2.localdomain>
2018-03-21 01:20:16 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -40,23 +40,23 @@ window.onload = function() {
         for (var i=0;i<imageData.data.length/4;i++) {
             var tDataVal = -10000 + ((imageData.data[i * 4] * 256 * 256 + imageData.data[i * 4 + 1] * 256 + imageData.data[i * 4 + 2]) * 0.1);
             // console.log('tDataVal ', tDataVal)
-            var alpha;
+            // var alpha;
 
-            if (tDataVal > 10) {
-                alpha = 0;
-            } else {
-                alpha = 100;
-            }
-            imageData.data[i * 4] = 10;
-            imageData.data[i*4+1] = 20;
-            imageData.data[i*4+2] = 200;
-            imageData.data[i*4+3] = alpha;
+            // if (tDataVal > 10) {
+            //     alpha = 0;
+            // } else {
+            //     alpha = 100;
+            // }
+            // imageData.data[i * 4] = 10;
+            // imageData.data[i*4+1] = 20;
+            // imageData.data[i*4+2] = 200;
+            // imageData.data[i*4+3] = alpha;
 
             dataArray[i] = tDataVal;
         }
-        tilesData[key] = dataArray
+
        delete imageData.data;
-        return tilesData
+        return {key, dataArray}
         })
         // imageObj = new Image();
         // imageObj.crossOrigin = 'Anonymous';
